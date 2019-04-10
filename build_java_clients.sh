821b24c800c6c64fd58e2b45b4934987ea16e896
David Kornel <kornys@outlook.com>
2019-04-10 10:11:41 +0200

    Update to java 11

--- build_java_clients.sh
+++ build_java_clients.sh
@@ -4,13 +4,10 @@ function install_java_client(){
     rm -rf ./build
     rm -rf ./cli-java
     mkdir -p build/java_clients
-    git clone https://github.com/rh-messaging/cli-java.git
-	cd cli-java
-	mvn package -B -DskipTests=true
-	cp ./cli-artemis-jms/target/cli-artemis-jms-*-LATEST.jar ../build/java_clients/cli-artemis-jms.jar
-	cp ./cli-qpid-jms/target/cli-qpid-jms-*-LATEST.jar ../build/java_clients/cli-qpid-jms.jar
-	cp ./cli-activemq/target/cli-activemq-*-LATEST.jar ../build/java_clients/cli-activemq.jar
-	cp ./cli-paho-java/target/cli-paho-java-*-SNAPSHOT-RELEASE.jar ../build/java_clients/cli-paho-java.jar
+    wget https://github.com/rh-messaging/cli-java/releases/download/v2.3.0er1/cli-activemq-1.2.2-SNAPSHOT-5.11.0.redhat-630371.jar -O build/java_clients/cli-activemq.jar
+    wget https://github.com/rh-messaging/cli-java/releases/download/v2.3.0er1/cli-artemis-jms-1.2.2-SNAPSHOT-2.6.3.redhat-00015.jar -O build/java_clients/cli-artemis-jms.jar
+    wget https://github.com/rh-messaging/cli-java/releases/download/v2.3.0er1/cli-qpid-jms-1.2.2-SNAPSHOT-0.40.0.redhat-00001.jar -O build/java_clients/cli-qpid-jms.jar
+    wget https://github.com/rh-messaging/cli-java/releases/download/v2.3.0er1/cli-paho-java-1.2.2-SNAPSHOT-RELEASE.jar -O build/java_clients/cli-paho-java.jar
 }
 
 install_java_client
